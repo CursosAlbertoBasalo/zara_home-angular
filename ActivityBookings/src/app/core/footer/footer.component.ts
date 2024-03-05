aeb4bdbc02b56cbf418c7f31bb21fa66b504b79a
Alberto Basalo <albertobasalo@hotmail.com>
2024-03-05 17:46:44 +0100

    feat: footer

--- ActivityBookings/src/app/core/footer/footer.component.ts
+++ ActivityBookings/src/app/core/footer/footer.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lab-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  author = {
+    name: 'Alberto',
+    url: 'https://albertobasalo.dev',
+  };
 
+  getYear() {
+    return new Date().getFullYear();
+  }
+
+  onAcceptCookiesClick() {
+    console.log('cookies accepted');
+  }
 }
--- ActivityBookings/src/app/core/header/header.component.ts
+++ ActivityBookings/src/app/core/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  title = 'ActivityBookings';
+  public title: string = 'ActivityBookings';
 }
