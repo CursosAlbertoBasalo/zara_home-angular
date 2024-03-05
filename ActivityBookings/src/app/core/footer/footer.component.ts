7447bac8b3750779e01c7085109a8273bbe19260
Alberto Basalo <albertobasalo@hotmail.com>
2024-03-05 18:21:17 +0100

    feat: year

--- ActivityBookings/src/app/core/footer/footer.component.ts
+++ ActivityBookings/src/app/core/footer/footer.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'lab-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
   author = {
-    name: 'Alberto',
+    name: 'Alberto Basalo',
     url: 'https://albertobasalo.dev',
   };
 
+  year = '';
+
+  constructor() {
+    console.log('construir footer');
+    this.author = { ...this.author, name: 'Ivan' };
+    this.year = this.getYear().toLocaleString();
+  }
+
   getYear() {
+    console.log('calculando año');
     return new Date().getFullYear();
   }
 
