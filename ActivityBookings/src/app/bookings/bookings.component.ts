4bf587a36f9d87534958098f772419b9eaf2339f
Alberto Basalo <albertobasalo@hotmail.com>
2024-03-07 16:08:09 +0100

    feat: bookings css

--- ActivityBookings/src/app/bookings/bookings.component.ts
+++ ActivityBookings/src/app/bookings/bookings.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ACTIVITIES } from '../shared/models/activities.data';
+import { Activity } from '../shared/models/activity.type';
 
 @Component({
   selector: 'lab-bookings',
   templateUrl: './bookings.component.html',
-  styleUrls: ['./bookings.component.css']
+  styleUrls: ['./bookings.component.css'],
 })
 export class BookingsComponent {
+  public activity: Activity = ACTIVITIES[1];
 
+  public currentParticipants: number = 3;
 }
