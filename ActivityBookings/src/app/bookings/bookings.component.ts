a613bb7426c6b760c392d7a5932df3f07d22bdaf
Alberto Basalo <albertobasalo@hotmail.com>
2024-03-07 17:06:25 +0100

    feat: entrada de datos

--- ActivityBookings/src/app/bookings/bookings.component.ts
+++ ActivityBookings/src/app/bookings/bookings.component.ts
@@ -8,7 +8,47 @@ import { Activity } from '../shared/models/activity.type';
   styleUrls: ['./bookings.component.css'],
 })
 export class BookingsComponent {
-  public activity: Activity = ACTIVITIES[1];
+  public activity: Activity = ACTIVITIES[3];
 
-  public currentParticipants: number = 3;
+  public currentParticipants: number = 2;
+
+  public newParticipants: number = 1;
+
+  public totalParticipants: number =
+    this.currentParticipants + this.newParticipants;
+
+  public maxNewParticipants: number =
+    this.activity.maxParticipants - this.currentParticipants;
+
+  public booked: boolean = false;
+
+  public getDisableBookingButton(): boolean {
+    return this.booked || this.newParticipants === 0;
+  }
+
+  // public getTotalParticipants(): number {
+  //   return this.currentParticipants + this.newParticipants;
+  // }
+
+  public onNewParticipantsChange(event: any) {
+    const input: HTMLInputElement = event.target;
+    const value = input.value;
+    console.log('el input ha cambiado', value);
+    this.newParticipants = parseInt(value, 10);
+    this.totalParticipants = this.currentParticipants + this.newParticipants;
+    //this.disableBookingButton = this.newParticipants === 0;
+  }
+
+  public onBookClick() {
+    console.log('Reservar actividad', this.totalParticipants);
+    this.booked = true;
+    if (this.totalParticipants === this.activity.maxParticipants) {
+      this.activity.status = 'sold-out';
+      return;
+    }
+    if (this.totalParticipants > this.activity.minParticipants) {
+      this.activity.status = 'confirmed';
+      return;
+    }
+  }
 }
